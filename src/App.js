d95ec3e7e3d013be0c2af2f63813f0bd82e34dee
Ankit <singh.statlion.ankit6@gmail.com>
2025-05-15 17:32:05 +0530

    tested

--- src/App.js
+++ src/App.js
@@ -2,30 +2,99 @@ import React, {useEffect,useState} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Section from "./components/Section/Section";
+import FilterSection from "./components/FilterSection/FilterSection";
+import {fetchTopAlbums,fetchNewAlbums,fetchSongs} from './api/api'
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [topAlbums,setTopAlbums]= useState([]);
+  const [topAlbumSongs,setTopAlbumSongs]= useState([]);
+  const [newAlbumSongs,setNewAlbumSongs]= useState([]);
 
+  const[songsData,setSongsData]=useState([]);
+  const[value,setValue]=useState(0);
+  const[filteredData,setFilteredData]=useState([]);
+
+  const generateTopAlbumSongs=async()=>{
+    try{
+      const res=await fetchTopAlbums();
+      setTopAlbumSongs(res);
+    }
+    catch(error){
+      console.log(error);
+      return null;
+    }
+  }
+
+  const generateNewAlbumSongs=async()=>{
+    try{
+      const res=await fetchNewAlbums();
+      setNewAlbumSongs(res);
+    }
+    catch(error){
+      console.log(error);
+      return null;
+    }
+  }
+
+  const generateSongs=async()=>{
+    try{
+      console.log("generateSongs");
+      const res=await fetchSongs();
+      setSongsData(res);
+      setFilteredData(res);
+    }
+    catch(error){
+      return null;
+    }
+  }
+
+  const generateNewSongs=(index)=>{
+    let key="";
+    
+    if(index===0){
+      return;
+    }
+    else if(index===1){
+      key="rock";
+    }
+    else if(index===2){
+      key="pop";
+    }
+    else if(index===3){
+      key="jazz";
+    }
+    else if(index===4){
+      key="blues";
+    }
+
+    let newSongsArray=songsData.filter((song)=>{
+      console.log("key: ",key)
+      return(song.genre.key===key);
+    })
+    console.log("generateNewSongs triggered and filtered this Data: ", newSongsArray)
+    setFilteredData(newSongsArray);
+  }
+
+  const handleChangeIndex=async(newValue)=>{
+    console.log("handleChangeIndex triggered with newValue: ",newValue)
+    setValue(newValue);
+    generateNewSongs(newValue);
+  }
   useEffect(()=>{
-    const fetchTopAlbums=async () => {
-      try{
-        const res = await axios.get("https://qtify-backend-labs.crio.do/albums/top");
-        setTopAlbums(res.data);
-      }catch(err){
-        console.error("Failed to fetch albums",err);
-      }
-    };
-
-    fetchTopAlbums();
-  },[]);
-
+    generateTopAlbumSongs();
+    generateNewAlbumSongs();
+    generateSongs();
+  },[])
   return (
     <div className="App">
       <Navbar />
       <Hero /> 
-      <Section type='album' title="Top Albums" data={topAlbums} />
+      <div className="sectionWrapper">
+      <Section type='album' title="Top Albums" data={topAlbumSongs} />
+      <Section type='album' title="New Albums" data={newAlbumSongs} />
+      <FilterSection type='song' title='Songs' value={value} filteredData={filteredData} handleChangeIndex={handleChangeIndex}/>
+    </div>
     </div>
   );
 }
